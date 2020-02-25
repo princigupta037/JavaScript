4e3ce0dbefac812c5a6f735baf2ef5b123822096
Princi Gupta <princygupta037@gmail.com>
2020-02-25 19:56:37 +0530

    Initial

--- external.js
+++ external.js
@@ -1,233 +1,336 @@
-
-console.log('Hello World :):)');
-
-
-# primitive datatypes
-
-var firstName='princi'
-console.log(firstName);
-
-var lastName='gupta';
-console.log(lastName);
-
-var age=24;
-console.log(age);
-
-var fullAge = true;
-console.log(fullAge);
-
-var  job;
-job ='teacher'
-console.log(job);
+// 65
+// console.log('Hello World :):)');
 
 
-var _2='two';
-console.log(_2);
+// # primitive datatypes
 
-var $3 = 'three';
-console.log($3);
+// var firstName='princi'
+// console.log(firstName);
 
-# not use reserved keywords as js variables
+// var lastName='gupta';
+// console.log(lastName);
 
-var function = 'p';
-console.log(function);
+// var age=24;
+// console.log(age);
 
-var firstName='princi'
-console.log(firstName);
+// var fullAge = true;
+// console.log(fullAge);
 
-var lastName='gupta';
-console.log(lastName);
-
-var age=24;
-console.log(age);
-
-var fullAge = true;
-console.log(fullAge);
-
-var  job;
-job ='teacher'
-console.log(job);
+// var  job;
+// job ='teacher'
+// console.log(job);
 
 
-var _2='two';
-console.log(_2);
+// var _2='two';
+// console.log(_2);
 
-var $3 = 'three';
-console.log($3);
+// var $3 = 'three';
+// console.log($3);
 
-# Basic Opeartors
+// # not use reserved keywords as js variables
 
-var year, yearPrinci, yearGupta ;
-year = 2010;
-yearPrinci = year-2;
-yearGupta = year*5;
-console.log(yearPrinci);
-console.log(yearGupta);
-console.log(year / 10);
-console.log(year + 10);
+// var function = 'p';
+// console.log(function);
 
-# Logical Operators
+// var firstName='princi'
+// console.log(firstName);
 
- agePrinci = 24;
- agePriya=25;
-var princiOlder = agePrinci > agePriya;
-console.log(princiOlder);
+// var lastName='gupta';
+// console.log(lastName);
 
-# Type of Operator
+// var age=24;
+// console.log(age);
 
-console.log(typeof agePrinci);
+// var fullAge = true;
+// console.log(fullAge);
 
-# Opeartor Precedence
-
-var nw = 2020;
-var yearPrinci = 1996;
-var fullAge = 24;
-
-var isFullAge = nw - yearPrinci >= fullAge ;
-console.log(isFullAge);
-
-#multiple group operators
-
-var age = (yearPrinci-fullAge) / 8;
-console.log(age);
+// var  job;
+// job ='teacher'
+// console.log(job);
 
 
-# Mulitple assignments
+// var _2='two';
+// console.log(_2);
 
-var x , y;
-x =y= (3*3) * 4-6;
-console.log(x,y);
+// var $3 = 'three';
+// console.log($3);
 
-// # More Operators
-x +=3;
-console.log(x);
+// # Basic Opeartors
 
-x++
-console.log(x);
-x--
-console.log(x);
+// var year, yearPrinci, yearGupta ;
+// year = 2010;
+// yearPrinci = year-2;
+// yearGupta = year*5;
+// console.log(yearPrinci);
+// console.log(yearGupta);
+// console.log(year / 10);
+// console.log(year + 10);
+
+// # Logical Operators
+
+//  agePrinci = 24;
+//  agePriya=25;
+// var princiOlder = agePrinci > agePriya;
+// console.log(princiOlder);
+
+// # Type of Operator
+
+// console.log(typeof agePrinci);
+
+// # Opeartor Precedence
+
+// var nw = 2020;
+// var yearPrinci = 1996;
+// var fullAge = 24;
+
+// var isFullAge = nw - yearPrinci >= fullAge ;
+// console.log(isFullAge);
+
+// #multiple group operators
+
+// var age = (yearPrinci-fullAge) / 8;
+// console.log(age);
 
 
-Assignment1(Calculating BMI)
+// # Mulitple assignments
+
+// var x , y;
+// x =y= (3*3) * 4-6;
+// console.log(x,y);
+
+// // # More Operators
+// x +=3;
+// console.log(x);
+
+// x++
+// console.log(x);
+// x--
+// console.log(x);
 
 
-var massPrinci=25;
-var massPriya=28;
-var heightPrinci=2;
-var heightPriya=1;
-var BMIPrinci = massPrinci  / (heightPrinci * heightPrinci) ;
-var BMIPriya = massPriya / (heightPriya * heightPriya) ;
-var result =  BMIPrinci > BMIPriya;
-console.log('is princi\s BMI is higher then Priya\s' +  result);
+// Assignment1(Calculating BMI)
 
 
-if else statement
+// var massPrinci=25;
+// var massPriya=28;
+// var heightPrinci=2;
+// var heightPriya=1;
+// var BMIPrinci = massPrinci  / (heightPrinci * heightPrinci) ;
+// var BMIPriya = massPriya / (heightPriya * heightPriya) ;
+// var result =  BMIPrinci > BMIPriya;
+// console.log('is princi\s BMI is higher then Priya\s' +  result);
 
-var fN = 'p'
-var lN='g'
-if (lN === 'gg'){
-    console.log(fN + 'is g');
-}else{
-    console.log(fN + 'will hv gg');
+
+// if else statement
+
+// var fN = 'p'
+// var lN='g'
+// if (lN === 'gg'){
+//     console.log(fN + 'is g');
+// }else{
+//     console.log(fN + 'will hv gg');
+// }
+
+// var fN =true;
+// var lN='g'
+// if (fN){
+//     console.log(fN + 'is g');
+// }else{
+//     console.log(fN + 'will hv gg');
+// }
+
+
+// Boolean logic
+
+// var firstname = 'princi';
+// var age = 20;
+
+// if(age< 13){
+//     console.log(firstname + ' is a girl.');
+// } else if( age >= 13 && age < 20 ){
+//     console.log(firstname + ' is a teenager ');
+// }else if(age >= 20 && age < 30)
+// {
+//     console.log(firstname + ' is a women.');
+// }
+
+// else{
+//     console.log(firstname + ' is a girl.');
+// }
+
+
+
+// # Ternary operator
+
+
+
+
+
+// var firstName ='princi'
+// var age =16;
+
+// age>=18 ? console.log(firstname + 'drinks beer .')
+// : console.log(firstname + ' drinks juice');
+
+
+// var drink = age>=18 ? 'beer' :'juice';
+// console.log(drink);
+
+// #Switch statement
+
+// var job='teacher';
+// switch(job){
+//     case 'teacher':
+//         console.log(firstname + ' teches kids how to code ');
+//         break;
+//     case 'driver':
+//         console.log(firstname + ' driver an uber ');
+//         break;
+//     case 'designer':
+//         console.log(firstname + ' designs beautiful websites');
+//         break;
+//     default:
+//         console.log(firstname +' does nothing');
+// }
+
+// // # replacing if else statement with switch statement
+// switch(true){
+//     case age>13:
+//         console.log(firstname+'A');
+//         break;
+//     case age>=13&& age<=18:
+//         console.log(firstname+'B');
+//         break;
+//     default:
+//         console.log(firstname+'C');
+// }
+
+
+// # Falsy values 
+
+// # Truthy values
+
+// var height;
+
+// height='';
+// if(height){
+//     console.log('variable is defined');
+// }else{
+//     console.log('not defined');
+// }
+
+// var height=0;
+// if(height){
+//     console.log('variable is defined');
+// }else{
+//     console.log('not defined');
+// }
+
+
+// #Equality opeartor
+
+// var height=23;
+// if(height===23){
+//     console.log('type coercion');
+// }
+
+// var p= {
+//     name:'princi',
+//     age:25,
+//     family:['mom','father','brothers','sister'],
+//     year:1996,
+//     yearAge:function(year) {
+//         return 1998 - year;
+//     }
+// }
+// console.log(p);
+// console.log(p.yearAge(1990));
+
+ 
+
+// var p = {
+//     name:'princi',
+//     age:25,
+//     family:['mom','father','brothers','sister'],
+//     year:1999,
+//     yearAge: function(){
+//         this.age= 2020 - this.year;
+//     }
+// }
+// p.yearAge();
+// console.log(p);
+
+// Loop & iterations
+
+// for(var i=0;i<10;i++){
+//     console.log(i);
+// }
+
+
+// var family =['mom','father','brothers','sister']
+// for (var i = 0; i < family.length; i++){
+//     console.log(family[i]);
+// }
+
+// while
+// var i = 0;
+// while(i<family.length){
+//     console.log(family[i]);
+//     i++;
+// }
+
+// CONTINUE
+// var family= ['mom',1990,'father','brothers','sister',1990]
+// for (var i = 0; i < family.length; i++){
+
+// if(typeof family[i] !== 'string') continue;
+//  console.log(family[i]);
+//   }
+
+// // // BREAK
+// // for (var i = 0; i < family.length; i++){
+// //     if(typeof family[i] !== 'string') break;
+// //     console.log(family[i]);
+// // }
+
+
+// //Loooping backwards
+
+// for (var i = family.length-1; i >=0; i--){
+    
+//     console.log(family[i]);
+// }
+
+
+// Hosting (Variable and function)
+
+
+// console.log(age);
+// var age=23;
+// console.log(age);
+
+
+// function foo(){
+//     var age = 65;
+//     console.log(age);
+// }
+// foo();
+// console.log(age);
+
+
+
+// Scoping & Lexical scoping & Hosting
+
+var a = 'hello';
+first();
+
+function first(){
+    var b = 'hi';
+    second();
+
+    function second(){
+        var c ='hey';
+        console.log( a + b + c );
+    }
+
 }
-
-var fN =true;
-var lN='g'
-if (fN){
-    console.log(fN + 'is g');
-}else{
-    console.log(fN + 'will hv gg');
-}
-
-
-Boolean logic
-
-var firstname = 'princi';
-var age = 20;
-
-if(age< 13){
-    console.log(firstname + ' is a girl.');
-} else if( age >= 13 && age < 20 ){
-    console.log(firstname + ' is a teenager ');
-}else if(age >= 20 && age < 30)
-{
-    console.log(firstname + ' is a women.');
-}
-
-else{
-    console.log(firstname + ' is a girl.');
-}
-
-
-
-# Ternary operator
-
-var firstName ='princi'
-var age =16;
-
-age>=18 ? console.log(firstname + 'drinks beer .')
-: console.log(firstname + ' drinks juice');
-
-
-var drink = age>=18 ? 'beer' :'juice';
-console.log(drink);
-
-
-Switch statement
-
-var job='teacher';
-switch(job){
-    case 'teacher':
-        console.log(firstname + ' teches kids how to code ');
-        break;
-    case 'driver':
-        console.log(firstname + ' driver an uber ');
-        break;
-    case 'designer':
-        console.log(firstname + ' designs beautiful websites');
-        break;
-    default:
-        console.log(firstname +' does nothing');
-}
-
-// # replacing if else statement with switch statement
-switch(true){
-    case age>13:
-        console.log(firstname+'A');
-        break;
-    case age>=13&& age<=18:
-        console.log(firstname+'B');
-        break;
-    default:
-        console.log(firstname+'C');
-}
-
-
-# Falsy values 
-
-# Truthy values
-
-var height;
-
-height='';
-if(height){
-    console.log('variable is defined');
-}else{
-    console.log('not defined');
-}
-
-var height=0;
-if(height){
-    console.log('variable is defined');
-}else{
-    console.log('not defined');
-}
-
-
-#Equality opeartor
-
-var height=23;
-if(height===23){
-    console.log('type coercion');
-}
-
--- function.js
+++ function.js
@@ -1,89 +1,92 @@
 
-#functions
+// #functions
 
-function calculateAge(birthyear){
-    return 2018-birthyear;
-}
+// function calculateAge(birthyear){
+//     return 2018-birthyear;
+// }
 
-var agePrinci = calculateAge(2020);
-var agePriya = calculateAge(2024);
-var agePg = calculateAge(2028);
-console.log(agePrinci,agePriya,agePg);
-
-
-function yearHello(year,firstName){
-    var age= calculateAge(year);
-    var retirement = 65 -age;
-    console.log(firstName + 'retires in ' + retirement);
-}
-
-yearHello(2020,'princi');
-
-#function expression
-
-var whatdoyoudo=function(job,firstName){
-    switch(job){
-        case 'teacher':
-            return firstName + 'teaches';
-        case 'driver':
-            return firstName + 'driver';
-        case 'designer':
-            return firstName + 'desines';
-        default:
-            return firstName + 'nothing';
-
-    }
-
-}
-;
-console.log(whatdoyoudo('teacher','princi'));
-console.log(whatdoyoudo('teacher','princi'));
-console.log(whatdoyoudo('retired','princi'));
+// var agePrinci = calculateAge(2020);
+// var agePriya = calculateAge(2024);
+// var agePg = calculateAge(2028);
+// console.log(agePrinci,agePriya,agePg);
 
 
+// function yearHello(year,firstName){
+//     var age= calculateAge(year);
+//     var retirement = 65 -age;
+//     console.log(firstName + 'retires in ' + retirement);
+// }
 
-// #function declaration
+// yearHello(2020,'princi');
 
-var whatdoyoudo=function(job,firstName){
+// #function expression
 
-}
+// var whatdoyoudo=function(job,firstName){
+//     switch(job){
+//         case 'teacher':
+//             return firstName + 'teaches';
+//         case 'driver':
+//             return firstName + 'driver';
+//         case 'designer':
+//             return firstName + 'desines';
+//         default:
+//             return firstName + 'nothing';
 
+//     }
 
-#Expressions( expects value in output)
-
-2+3
-
-# Statement(expects no value)
-
-if(true){
-    console.log(23);
-}
-
-# arrays
-
-var names = ['princi', 'Priya','vidhya'];
-var years = new Array(1995,1994,1996);
-console.log([0]);
-console.log(names);
-console.log(names.length);
-
-
-# Mutate array data
-names[3] = 'vaishali';
-console.log(names);
-
-# Different data types
-
-var princi = ['princi','Gupta',1995,'Developer','false'];
-princi.push('hello');
+// }
+// ;
+// console.log(whatdoyoudo('teacher','princi'));
+// console.log(whatdoyoudo('teacher','princi'));
+// console.log(whatdoyoudo('retired','princi'));
 
 
-console.log(princi);
-princi.pop();
-console.log(princi);
-princi.shift();
-console.log(princi);
-console.log(princi.indexOf(1996));
 
-var isDeveloper= princi.indexOf('Developer') === -1 ? 'princi is not a developer' : 'princi is a developer' ;
-console.log(isDeveloper);
+// // #function declaration
+
+// var whatdoyoudo=function(job,firstName){
+
+// }
+
+
+// #Expressions( expects value in output)
+
+// 2+3
+
+// # Statement(expects no value)
+
+// if(true){
+//     console.log(23);
+// }
+
+// # arrays
+
+// var names = ['princi', 'Priya','vidhya'];
+// var years = new Array(1995,1994,1996);
+// console.log([0]);
+// console.log(names);
+// console.log(names.length);
+
+
+// # Mutate array data
+// names[3] = 'vaishali';
+// console.log(names);
+
+// # Different data types
+
+// var princi = ['princi','Gupta',1995,'Developer','false'];
+// princi.push('hello');
+
+
+// console.log(princi);
+// princi.pop();
+// console.log(princi);
+// princi.shift();
+// console.log(princi);
+// console.log(princi.indexOf(1996));
+
+// var isDeveloper= princi.indexOf('Developer') === -1 ? 'princi is not a developer' : 'princi is a developer' ;
+// console.log(isDeveloper);
+
+
+// objects
